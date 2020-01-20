ef01d763bc27a7e7c945509c1baf1e9c2ea7d05b
Samuel Roque <samuelroquech@gmail.com>
2020-01-20 22:34:59 +0000

    Vue

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,24 +1,74 @@
 <template>
   <div>
-    <h1 class="justify-center text-4xl my-5">Añade Temas</h1>
-    <form class="my-20 flex justify-center">
-      <section class="">
+    <h1 class="justify-center text-4xl my-5 text-center">{{title}}</h1>
+    <div class="flex w-full justify-center">
+      <form class="my-10 block w-6/12 justify-center">
         <div>
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="topic">
-            <span>Tema: </span>
-            <strong><abbr title="required">*</abbr></strong>
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="topic"
+          >
+            <span>Tema:</span>
+            <strong>
+              <abbr title="required">*</abbr>
+            </strong>
           </label>
-          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" id="topic" name="topic">
+          <input
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="text"
+            id="topic"
+            name="topic"
+          />
         </div>
         <div>
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="text">
-            <span> Texto: </span>
-            <strong><abbr title="required">*</abbr></strong>
+          <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="text"
+          >
+            <span>Texto:</span>
+            <strong>
+              <abbr title="required">*</abbr>
+            </strong>
           </label>
-          <textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="text" name="text"></textarea>
+          <textarea
+            class="appearance-none h-56 block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="text"
+            name="text"
+          ></textarea>
         </div>
-      </section>
-    </form>  
+        <div class="md:flex md:items-center">
+          <div class="w-1/3"></div>
+          <div class="w-2/3 text-right">
+            <button
+              class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+              v-on:click="saveText"
+            >Enviar</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "default",
+  data() {
+    return {
+      title: "Añade Temas"
+    };
+  },
+  methods: {
+    saveText: function(event) {
+      // `this` inside methods points to the Vue instance
+      alert("Hello " + this.title + "!");
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName);
+      }
+    }
+  }
+};
+</script>
 
