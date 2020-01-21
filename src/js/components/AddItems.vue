53ce4d060c08735475711a7a8931cb9a78efff58
Samuel Roque - Codebay Innovation <s.concepcion@codebay-innovation.com>
2020-01-21 16:20:24 +0000

    List

--- src/js/components/AddItems.vue
+++ src/js/components/AddItems.vue
@@ -41,7 +41,7 @@
           <div class="w-1/3"></div>
           <div class="w-2/3 text-right">
             <button
-              class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              class="shadow bg-teal-500 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="submit"
             >Enviar</button>
           </div>
--- src/js/components/ListItems.vue
+++ src/js/components/ListItems.vue
@@ -1,47 +1,38 @@
 <template>
   <div>
     <h1 class="justify-center text-4xl my-5 text-center">{{title}}</h1>
-    
+    <div class="flex w-full justify-center">
+      <table class="table-fixed">
+        <thead>
+          <tr>
+            <th class="w-1/2 px-4 py-2">Tema</th>
+            <th class="w-1/4 px-4 py-2">Texto</th>
+            <th class="w-1/4 px-4 py-2">Operaciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in items" class="bg-gray-100">
+            <td class="border px-4 py-2">{{ item.tags }}</td>
+            <td class="border px-4 py-2">{{ item.text }}</td>
+            <td class="border px-4 py-2">858</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "default",
+  props: ['items'],
   data() {
     return {
       title: "Lista de  Temas",
-      items: ""
     };
   },
-  mounted() {
-    if(localStorage.items) {
-      this.items = JSON.parse(localStorage.items);
-    }else{
-      this.items = [];
-    }
-  }, 
-  watch: {
-    items(newItem) {
-      localStorage.items = JSON.stringify(newItem);
-    }
-  },
   methods: {
-    addItem: function(tags, text){
-      if(Array.isArray(this.items)){
-        this.items.push({
-          tags : tags,
-          text : text
-        });  
-      }
-    },
-    saveText: function(event) {
-      if(this.topic == "" || this.text == ""){
-        alert("Por favor, rellena todos los campos");
-        return;
-      }
-      this.addItem(this.topic, this.text);
-    }
+    
   }
 };
 </script>
